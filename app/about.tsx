745866fe6ab7b063d7e906eff4080ba3f93fe825
KellyNyanbianry <43101126+KellyNyanbinary@users.noreply.github.com>
2026-01-26 00:33:19 -0700

    Add additional options to the select field component on the about page

--- app/about.tsx
+++ app/about.tsx
@@ -98,6 +98,11 @@ export default function About() {
     { label: 'Option 3', value: 'option-3' },
     { label: 'Option 4', value: 'option-4' },
     { label: 'Option 5', value: 'option-5' },
+    { label: 'Option 6', value: 'option-6' },
+    { label: 'Option 7', value: 'option-7' },
+    { label: 'Option 8', value: 'option-8' },
+    { label: 'Option 9', value: 'option-9' },
+    { label: 'Option 10', value: 'option-10' },
   ];
   const buttonRows: ButtonRow[] = [
     {
