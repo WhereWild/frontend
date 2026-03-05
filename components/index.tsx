52c5bd53f188031bdc4867f39a4e80812ced98de
KellyNyanbianry <43101126+KellyNyanbinary@users.noreply.github.com>
2026-03-05 21:10:52 +0000

    106 Implement top app bar

--- components/index.tsx
+++ components/index.tsx
@@ -28,6 +28,9 @@ export type { DataEntrySectionEntry, DataEntrySectionProps } from './lists/DataE
 export { WebPageHeader } from './sections/webPageHeader/WebPageHeader';
 export type { WebPageHeaderAction, WebPageHeaderProps } from './sections/webPageHeader/WebPageHeader';
 
+export { TopAppBar } from './sections/topAppBar/TopAppBar';
+export type { TopAppBarProps, TopAppBarVariant } from './sections/topAppBar/TopAppBar';
+
 export { SearchInput } from './inputs/SearchInput';
 export type { SearchInputProps } from './inputs/SearchInput';
 
