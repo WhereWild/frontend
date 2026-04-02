0e3f829ec1cb0532e608fe2378985ecbad41528c
KellyNyanbianry <43101126+KellyNyanbinary@users.noreply.github.com>
2026-04-02 19:34:04 +0000

    Add hover fade transitions for buttons

---
+++ test-utils/withPlatformOS.ts
@@ -0,0 +1,53 @@
+import { Platform } from 'react-native';
+
+type WithPlatformOptions = {
+  ensureWebWindowListeners?: boolean;
+};
+
+export const withPlatformOS = (
+  platform: typeof Platform.OS,
+  run: () => void,
+  options?: WithPlatformOptions,
+) => {
+  const originalPlatform = Platform.OS;
+  const windowObject = global.window as (Window & typeof globalThis) | undefined;
+  const originalAddEventListener = windowObject?.addEventListener;
+  const originalRemoveEventListener = windowObject?.removeEventListener;
+  const hadAddEventListener = windowObject != null && 'addEventListener' in windowObject;
+  const hadRemoveEventListener = windowObject != null && 'removeEventListener' in windowObject;
+  const fallbackAddEventListener = jest.fn() as typeof window.addEventListener;
+  const fallbackRemoveEventListener = jest.fn() as typeof window.removeEventListener;
+
+  Object.defineProperty(Platform, 'OS', {
+    configurable: true,
+    value: platform,
+  });
+
+  if (platform === 'web' && options?.ensureWebWindowListeners && windowObject) {
+    windowObject.addEventListener = originalAddEventListener ?? fallbackAddEventListener;
+    windowObject.removeEventListener = originalRemoveEventListener ?? fallbackRemoveEventListener;
+  }
+
+  try {
+    run();
+  } finally {
+    Object.defineProperty(Platform, 'OS', {
+      configurable: true,
+      value: originalPlatform,
+    });
+
+    if (platform === 'web' && options?.ensureWebWindowListeners && windowObject) {
+      if (hadAddEventListener && originalAddEventListener) {
+        windowObject.addEventListener = originalAddEventListener;
+      } else {
+        delete (windowObject as Partial<Window>).addEventListener;
+      }
+
+      if (hadRemoveEventListener && originalRemoveEventListener) {
+        windowObject.removeEventListener = originalRemoveEventListener;
+      } else {
+        delete (windowObject as Partial<Window>).removeEventListener;
+      }
+    }
+  }
+};
