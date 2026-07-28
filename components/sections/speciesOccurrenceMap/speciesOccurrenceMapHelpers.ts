33ce00a6f150f4e53893409574f2991e54f38234
Lucas Pearce <u1110118@utah.edu>
2026-07-28 14:26:35 -0600

    Revert "lint"
    This reverts commit e18c55e523df01b43ba803e26c7b9a551df1f1d3.

--- components/sections/speciesOccurrenceMap/speciesOccurrenceMapHelpers.ts
+++ components/sections/speciesOccurrenceMap/speciesOccurrenceMapHelpers.ts
@@ -597,9 +597,7 @@ export const buildGlobeHtml = (...args: FillMapTemplateArgs): string => {
     classShapes,
     markerOutlineEnabled,
     circularShapesEnabled,
-    labelsOverlayUrl
-      ? stripRetinaPlaceholder(labelsOverlayUrl)
-      : labelsOverlayUrl,
+    labelsOverlayUrl ? stripRetinaPlaceholder(labelsOverlayUrl) : labelsOverlayUrl,
     linesOverlayUrl ? stripRetinaPlaceholder(linesOverlayUrl) : linesOverlayUrl,
     locationPickerMode,
     initialLocalLat,
